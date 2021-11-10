d07a9846e22959d448f31fbc4609ea7550a1c784
andmalt <andrea.maltempi@gmail.com>
2021-11-10 21:59:01 +0100

    inserted model and migration Comic

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -2,18 +2,29 @@
 
 @section('content')
 
-<div class="container">
+<div class="container pt-5 p-md-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <h3>prova</h3>
-                </div>
-
-                <div class="card-body">
-                    <p>provaaaaaaaaa</p>
-                </div>
-            </div>
+        <div class="col-12 col-md-11 col-lg-10">
+            <table class="table table-dark table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col"> Titolo </th>
+                        <th scope="col">Autore</th>
+                        <th scope="col">Data di pubblicazione</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                    <tr>
+                        <td scope="row"></td>
+                        <td><a href="{{ route('comics.show', $comic->id )}}" class="link_name">{{strtoupper($comic->title)}}</a></td>
+                        <td>{{$comic->author}}</td>
+                        <td>{{date("d-m-Y",strtotime($comic->publication_date))}}</td>
+                    </tr> 
+                    @endforeach             
+                </tbody> 
+            </table>
         </div>
     </div>
 </div>
---
+++ resources/views/admin/comics/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', $comic->title)
+
+@section('content')
+
+<div class="container p-5">
+    <div class="row">
+        <div class="col-12 col-md-10">
+            <div class="">
+                <div class="card mb-3">
+                    <img src="{{ $comic->image_url}}" class="card-img-top img-fluid img_card" alt="{{$comic->title}} image">
+                    <div class="card-body">
+                      <h5 class="card-title"><strong>{{strtoupper($comic->title)}}</strong></h5>
+                      <p class="card-text"><strong>Autore:</strong>  {{$comic->author}}</p>
+                      <p class="card-text"><strong>Data di pubblicazione:</strong>   {{date("d-m-Y",strtotime($comic->publication_date))}}</p>
+                      <p class="card-text"><strong>Descrizione:</strong>  {{$comic->description}}</p>
+                      <p class="card-text"><small class="text-muted">Last updated {{date("H:i:s d-m-Y",strtotime($comic->updated_at))}}</small></p>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
