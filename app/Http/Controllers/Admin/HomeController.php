f21e00f4838e47498b9bc0c03438275e93536ade
andmalt <andrea.maltempi@gmail.com>
2021-11-10 18:30:34 +0100

    inserted admin controller

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -16,13 +17,9 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('welcome');
+    
+    public function index(){
+        return view('admin.home');
     }
+ 
 }
